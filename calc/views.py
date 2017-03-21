73e14f03f18e13327d0b0fa4d4fb9e999f466e33
javierfm27 <javitofm27@github.com>
2017-03-21 10:21:49 +0100

    Ejercicio inacabado, queda division

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def suma(request, operando1, operacion, operando2):
+    resultado = int(operando1) + int(operando2)
+    htmlAnswer = '<p> El resultado--> ' + operando1 + " " + operacion \
+                + " " + operando2 + " = " + str(resultado) + "</p>"
+    return HttpResponse(htmlAnswer)
+
+def mult(request, operando1, operacion, operando2):
+    resultado = int(operando1) * int(operando2)
+    htmlAnswer = "<p> El resultado--> " + operando1 + " " + operacion \
+                + " " + operando2 + " = " + str(resultado) + "</p>"
+    return HttpResponse(htmlAnswer)
+
+def div(request, operando1, operacion, operando2):
+    try:
+        resultado = int(operando1) / int(operando2)
+        htmlAnswer = "<p> El resultado--> " + operando1 + " " + operacion \
+                    + " " + operando2 + " = " + str(resultado) + "</p>"
+        return HttpResponse(htmlAnswer)
+    except ZeroDivisionError:
+        hmtlAnswer = "<h3> ERROR! Ha intentado dividir por cero </h3>"
+        return HttpResponse.HttpResponseGone(htmlAnswer)
+
+def sub(request, operando1, operacion, operando2):
+        resultado = int(operando1) - int(operando2)
+        htmlAnswer = "<p> El resultado--> " + operando1 + " " + operacion \
+                    + " " + operando2 + " = " + str(resultado) + "</p>"
+        return HttpResponse(htmlAnswer)
--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'(\d+)(\+)(\d+)','calc.views.suma',),
+    url(r'(\d+)(\*)(\d+)','calc.views.mult',),
+    url(r'(\d+)(\\)(\d+)','calc.views.div',),
+    url(r'(\d+)(-)(\d+)','calc.views.sub',),
     url(r'^admin/', include(admin.site.urls)),
 )
